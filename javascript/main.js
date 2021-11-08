58072434ca2dbfb788818107dde25d1c0f8c995b
Liana Rogers <li.rogers.00@gmail.com>
2021-11-08 15:25:08 -0800

    javascript coding attempts

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+function validateForm() {
+    alert("its working");
+    var status = true;
+    var x;
+
+    x = document.form.myForm.fname.value;
+    console.log(x);
+
+    return status;
+}
+//end of the form
